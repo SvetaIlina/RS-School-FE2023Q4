4ac4f899884e3e1604b95ff982f18c6c615c011c
Svetlana Ilina <svetailina1987@gmail.com>
2023-11-29 18:28:57 +0400

    feat: add style and  functional for burger menu

---
+++ coffee-house/js/app.js
@@ -0,0 +1,38 @@
+//Burger
+
+const burgerBtn = document.querySelector('.burger');
+const menuLinks = document.querySelectorAll('.menu__list-link');
+const menu = document.querySelector('.menu__list');
+
+
+
+
+burgerBtn.addEventListener('click', showBurgerMenu);
+menuLinks.forEach(link => {
+    link.addEventListener('click', closeBurger);
+
+});
+
+
+function showBurgerMenu () {
+    if (burgerBtn.classList.contains('burger--active')) {
+        closeBurger()
+    } else {
+        openBurger()
+            }
+  }
+
+  function openBurger() {
+    burgerBtn.classList.add('burger--active');
+    menu.classList.add('menu__list--active');
+    document.body.style.overflow = 'hidden'
+  }
+
+  function closeBurger() {
+    burgerBtn.classList.remove('burger--active');
+    menu.classList.remove('menu__list--active');
+    document.body.style.overflow = ''
+  }
+
+
+
