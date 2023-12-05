fa12ea4d41df1260e3d6eeab47ef9dfe6a605188
Svetlana Ilina <svetailina1987@gmail.com>
2023-12-05 15:19:24 +0400

    feat: add Modal class

--- coffee-house/js/app.js
+++ coffee-house/js/app.js
@@ -1,6 +1,8 @@
 'use strict'
 import myData from "./product.json" assert { type: "json" };
 
+
+
 //Burger
 
 const burgerBtn = document.querySelector('.burger');
@@ -40,7 +42,8 @@ function showBurgerMenu () {
 
 // render Menu
 
-
+const parentTab = document.querySelector('.tabs__content');
+parentTab.addEventListener('click', showModal)
 
 class MenuCard {
   constructor ({id, url, name, description, price, category}) {
@@ -106,7 +109,7 @@ function hideExtraCard () {
 }
 
   function renderMenu() {
-const parentTab = document.querySelector('.tabs__content');
+// const parentTab = document.querySelector('.tabs__content');
     parentTab.innerHTML = ''
   const cardsArray = createMenuCard(myData);
   const tabsCont = createMenuTab(cardsArray);
@@ -211,7 +214,7 @@ mediaQuery.addListener(handleTabletChange)
 
 function handleTabletChange(e) {
   
-  if (e.matches && document.querySelector('show').childNodes.length > 4) {
+  if (e.matches && document.querySelector('.show').childNodes.length > 4) {
 
     hideExtraCard ();
     
@@ -219,8 +222,144 @@ function handleTabletChange(e) {
 }
 
 
+///Modal
+
+class Modal {
+  constructor ({id, url, name, description, price, sizes, additives}) {
+    this.id = id;
+    this.url = url;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.sizes = sizes;
+    this.additives = additives;
+        
+}
+
+ generateModal () {
+  const modalCard = document.createElement('div');
+  
+  modalCard.classList.add('modal__inner');
+  modalCard.innerHTML = `
+                    <div class="modal-img">
+                        <div class="modal-img__wrapper">
+                          <img src=${this.url} alt=${this.name}>
+                        </div>
+                    </div>
+  `
+  
+  modalCard.append(this.createContent());
+  modalCard.addEventListener('click', this.changeSum)
+return modalCard;
 
 
+}
+
+createContent() {
+  const content = document.createElement('div');
+  content.classList.add('modal-content');
+  content.innerHTML = `
+                        <div class="item__descr">
+                            <h3 class="item-title modal-title">${this.name}</h3>
+                            <p class="section-text">${this.description}</p>
+                        </div>
+  `
+  content.append(this.createSizeBtn())
+  content.append(this.createAdditivesBtn())
+
+  content.innerHTML += `
+  <div class="item__total">
+  <h3 class="item-title">Total:</h3>
+  <p class="item-price">$${this.price}</p>
+  </div>
+  <p class="warning">The cost is not final. Download our mobile app to see the final price and place your order. Earn loyalty points and enjoy your favorite coffee with up to 20% discount.</p>
+  <button class="close-modal">Close</button>
+  `
+    
+  return content
+}
+
+
+ createSizeBtn() {
+  const itemSizes = document.createElement('div')
+  itemSizes.classList.add('item__size')
+  itemSizes.innerHTML = '<h5 class="section-text">Size</h5>'
+  const sizeBtns = []
+
+  for ( let key in this.sizes) {
+       const btn =  document.createElement('button');
+       btn.classList.add('modal__item-btn');
+       if(key == "s") {
+        btn.classList.add('modal__item-btn--active');
+       }
+       btn.innerHTML = `
+       <span class="choose">${key}</span>
+       ${this.sizes[key]["size"]}
+       `
+       sizeBtns.push(btn)
+
+  }
+
+  sizeBtns.forEach( size => itemSizes.append(size));
+  return itemSizes;
+}
+
+ createAdditivesBtn() {
+
+  const itemAdditives = document.createElement('div')
+  itemAdditives.classList.add('item__Additives')
+  itemAdditives.innerHTML = '<h5 class="section-text">Additives</h5>'
+  const additivesBtns = []
+
+  this.additives.forEach ((item, i) => {
+    const btn =  document.createElement('button');
+    btn.classList.add('modal__item-btn');
+    btn.innerHTML = `
+    <span class="choose">${i+1}</span>
+    ${item["name"]}
+    `
+    additivesBtns.push(btn)
+  })
+
+  additivesBtns.forEach (add => itemAdditives.append(add));
+ 
+
+
+  return itemAdditives;
+}
+
+
+ changeSum (e) {
+  const targetBtn = e.target.closest('.modal__item-btn');
+ 
+
+}
+
+}
+
+function showModal (e) {
+ const targetCard = e.target.closest('.item-card');
+ const dataId = targetCard.getAttribute('data-id');
+ const dataForModal = myData.filter(i => i.id == dataId);
+ const modal = new Modal(dataForModal[0])
+ const overlay = document.createElement('div');
+ overlay.classList.add('modal');
+ overlay.append(modal.generateModal());
+ document.body.append(overlay);
+ document.body.style.overflow = 'hidden';
+
+ overlay.addEventListener('click', closeModal);
+
+}
+
+
+function closeModal(e) {
+  
+  if(e.target.classList.contains('close-modal') || e.target.classList.contains('modal') ) {
+    document.querySelector('.modal').remove();
+    document.body.style.overflow = '';
+}
+}
 
 
 
