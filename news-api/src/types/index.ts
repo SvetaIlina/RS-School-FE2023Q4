9fe75ee3fddf8e3fab43ae5db98b9eaab469da9a
Svetlana Ilina <svetailina1987@gmail.com>
2024-02-19 17:45:33 +0400

    feat: add new interfaces

---
+++ news-api/src/types/index.ts
@@ -0,0 +1,31 @@
+export interface INews {
+    urlToImage: string;
+    author: string;
+    source: { name: string; id: string };
+    publishedAt: string;
+    title: string;
+    description: string;
+    url: string;
+    content: string;
+}
+
+export interface INewsResponse {
+    status: string;
+    totalResults: number;
+    articles: INews[];
+}
+
+export interface ISources {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+export interface ISourcesResponse {
+    status: string;
+    sources: ISources[];
+}
