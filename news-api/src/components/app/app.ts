aecbe838da6df4687b11644d7df2644391aa3d5e
Svetlana Ilina <svetailina1987@gmail.com>
2024-02-25 14:16:00 +0400

    feat: add data types in start method

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -1,5 +1,6 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { ISourcesResponse, INewsResponse } from '../../types/servise';
 
 class App {
     controller: AppController;
@@ -12,8 +13,10 @@ class App {
     start() {
         document
             .querySelector('.sources')!
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+            .addEventListener('click', (e) =>
+                this.controller.getNews(e, (data: INewsResponse) => this.view.drawNews(data))
+            );
+        this.controller.getSources((data: ISourcesResponse) => this.view.drawSources(data));
     }
 }
 
