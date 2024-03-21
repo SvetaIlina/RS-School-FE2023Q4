6aff4bd0fe448428ae00e5372a196d2dc5a1a939
Svetlana Ilina <svetailina1987@gmail.com>
2024-03-21 12:38:12 +0400

    init: add projects enviroment

---
+++ async-race/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    plugins: ['prettier', 'import', '@typescript-eslint'],
+    extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.ts', '.js'],
+            },
+        },
+    },
+    rules: {
+        '@typescript-eslint/no-explicit-any': 2,
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+    },
+};
