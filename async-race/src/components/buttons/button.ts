11d28699c18a86adc900b5185f36f659ce5c2bb4
Svetlana Ilina <svetailina1987@gmail.com>
2024-03-21 15:36:03 +0400

    feat: change button constructor

--- async-race/src/components/buttons/button.ts
+++ async-race/src/components/buttons/button.ts
@@ -1,16 +1,14 @@
 import BaseComponent from '../baseComponent';
-import { Callback } from '../../type/types';
-import './btn.css';
 
-export default class Button<T = undefined> extends BaseComponent {
-    constructor(btnClasses: Array<string>, btnText: string, btnCallback: Callback<T>, arg: T) {
+export default class Button extends BaseComponent {
+    constructor(btnClasses: Array<string>, btnText: string, btnCallback: () => void) {
         super({
             tag: 'button',
             classes: btnClasses,
             textContent: btnText,
             onClick: (event) => {
                 event.preventDefault();
-                btnCallback(arg);
+                btnCallback();
             },
             eventType: 'click',
         });
