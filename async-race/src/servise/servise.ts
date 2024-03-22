bd7b15e5d9b145ff2cc49f42c95b260a6b2c4fa4
Svetlana Ilina <svetailina1987@gmail.com>
2024-03-22 19:03:29 +0400

    feat: add separate checking functions

--- async-race/src/servise/servise.ts
+++ async-race/src/servise/servise.ts
@@ -1,15 +1,15 @@
-// export function isNotNull<T>(value: unknown): asserts value is NonNullable<T> {
-//     if (value === null || value === undefined) {
-//         throw new Error(`Not expected value: ${value}`);
-//     }
-// }
-// export function isHTMLElement(value: unknown): asserts value is HTMLElement {
-//     if (!(value instanceof HTMLElement)) {
-//         throw new Error(`Expected value to be of type HTMLElement`);
-//     }
-// }
+export function isNotNull<T>(value: unknown): asserts value is NonNullable<T> {
+    if (value === null || value === undefined) {
+        throw new Error(`Not expected value: ${value}`);
+    }
+}
+export function isHTMLElement(value: unknown): asserts value is HTMLElement {
+    if (!(value instanceof HTMLElement)) {
+        throw new Error(`Expected value to be of type HTMLElement`);
+    }
+}
 
-export default function isNotNullHTMLElement<T>(value: unknown): asserts value is T {
+export function isNotNullHTMLElement<T>(value: unknown): asserts value is T {
     if (value === null || value === undefined || !(value instanceof HTMLElement)) {
         throw new Error(`Not expected value: ${value}`);
     }
