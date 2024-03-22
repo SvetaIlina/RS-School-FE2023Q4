9b4796d5690e770c7d7d89620f83699c6d7bcdb0
Svetlana Ilina <svetailina1987@gmail.com>
2024-03-22 16:05:52 +0400

    feat:  add View class

---
+++ async-race/src/components/view.ts
@@ -0,0 +1,18 @@
+import BaseComponent from './baseComponent';
+import { elemOptions } from '../type/types';
+
+export default class View {
+    view: BaseComponent;
+
+    constructor(options: Pick<elemOptions, 'tag' | 'classes'>) {
+        this.view = new BaseComponent({
+            tag: options.tag,
+            classes: options.classes,
+            textContent: '',
+        });
+    }
+
+    getViewElement() {
+        return this.view.getElement();
+    }
+}
