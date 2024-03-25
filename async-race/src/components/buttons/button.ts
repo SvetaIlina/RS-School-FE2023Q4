3eca416aceb572df2d5ce3898a8efcb95d14596c
Svetlana Ilina <svetailina1987@gmail.com>
2024-03-25 20:03:40 +0400

    fix: callback type

--- async-race/src/components/buttons/button.ts
+++ async-race/src/components/buttons/button.ts
@@ -1,15 +1,12 @@
 import BaseComponent from '../baseComponent';
 
 export default class Button extends BaseComponent {
-    constructor(btnClasses: Array<string>, btnText: string, btnCallback: () => void) {
+    constructor(btnClasses: Array<string>, btnText: string, btnCallback: (event: Event) => void) {
         super({
             tag: 'button',
             classes: btnClasses,
             textContent: btnText,
-            onClick: (event) => {
-                event.preventDefault();
-                btnCallback();
-            },
+            onClick: btnCallback,
             eventType: 'click',
         });
     }
