15d37bd097ecd629c2b0d691ba3b01cf983f0324
Svetlana Ilina <svetailina1987@gmail.com>
2024-03-27 12:39:01 +0400

    feat: add displaying view

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,1 +1,9 @@
 import './style.css';
+
+import HeaderView from './components/header/header';
+import MainView from './components/main/main';
+
+const main = new MainView();
+const header = new HeaderView(main);
+
+document.body.append(header.getViewElement(), main.getViewElement());
