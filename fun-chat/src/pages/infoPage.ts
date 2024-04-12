f76ba61036ecb56219af3d323bf92107464d558e
Svetlana Ilina <svetailina1987@gmail.com>
2024-04-12 16:41:14 +0400

    feat: add InfoPage

---
+++ fun-chat/src/pages/infoPage.ts
@@ -0,0 +1,42 @@
+import BaseComponent from '../components/baseComponent';
+import Button from '../components/button';
+import BasePage from './basePage';
+
+export default class InfoPage extends BasePage {
+    constructor() {
+        super();
+        this.configView();
+    }
+
+    configView() {
+        const wrapper = new BaseComponent({
+            tag: 'div',
+            classes: ['infoWrapper'],
+        });
+        const title = new BaseComponent({
+            tag: 'h1',
+            classes: ['infoTitle'],
+            textContent: 'Fun Chat',
+        });
+
+        const text = new BaseComponent({
+            tag: 'p',
+            classes: ['infoText'],
+            textContent:
+                'Welcome to the chat room! This chat room has been created as part of the RSSchool JS/FE 2023Q3 course. Here you can chat with other users, send messages, and share your thoughts. Please remember that this is a learning project and we value your participation and feedback. Enjoy your conversations!',
+        });
+
+        const myLink = new BaseComponent({
+            tag: 'a',
+            classes: ['infoLink'],
+            textContent: 'Author Svetlana Ilina',
+            attributes: [{ key: 'href', value: 'https://github.com/SvetaIlina' }],
+        });
+
+        const backBtn = new Button(['backBtn'], 'Back', () => {
+            this.notifyObservers('back');
+        });
+        wrapper.addChild([title, text, myLink, backBtn]);
+        this.addChild([wrapper]);
+    }
+}
