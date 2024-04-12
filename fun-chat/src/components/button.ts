ef8fc2c9c73d1fffa4aba8a48deef2d52385907d
Svetlana Ilina <svetailina1987@gmail.com>
2024-04-12 14:16:16 +0400

    feat: add type attribute in btn constructor

--- fun-chat/src/components/button.ts
+++ fun-chat/src/components/button.ts
@@ -1,12 +1,15 @@
 import BaseComponent from './baseComponent';
 
 export default class Button extends BaseComponent {
-    constructor(btnClasses: Array<string>, btnText: string, btnCallback: (event: Event) => void) {
+    constructor(btnClasses: Array<string>, btnText: string, btnCallback: (event: Event) => void, btnType?: string) {
         super({
             tag: 'button',
             classes: btnClasses,
             textContent: btnText,
         });
         this.setCallback(btnCallback, 'click');
+        if (btnType) {
+            this.setAttributes([{ key: 'type', value: `${btnType}` }]);
+        }
     }
 }
