145a20e4585ce530c0738fc32a4199c68c5ba32f
Svetlana Ilina <svetailina1987@gmail.com>
2024-04-12 16:40:17 +0400

    feat: add Base Page component

---
+++ fun-chat/src/pages/basePage.ts
@@ -0,0 +1,27 @@
+import BaseComponent from '../components/baseComponent';
+import MainView from '../view';
+
+export default class BasePage extends BaseComponent {
+    private observers: MainView[] = [];
+
+    constructor() {
+        super({
+            tag: 'div',
+            classes: ['page'],
+        });
+    }
+
+    subscribe(observer: MainView) {
+        this.observers.push(observer);
+    }
+
+    protected notifyObservers(action: string) {
+        this.observers.forEach((observer) => {
+            observer.update(action);
+        });
+    }
+
+    getPageElement() {
+        return this.getElement();
+    }
+}
--- fun-chat/src/pages/loginPage.ts
+++ fun-chat/src/pages/loginPage.ts
@@ -2,14 +2,12 @@ import BaseComponent from '../components/baseComponent';
 import LoginForm from '../components/loginForm/loginForm';
 import Button from '../components/button';
 import LoginInput from '../components/loginForm/input/inputField';
+import BasePage from './basePage';
 
-export default class LoginPage extends BaseComponent {
+export default class LoginPage extends BasePage {
     form: LoginForm;
     constructor() {
-        super({
-            tag: 'div',
-            classes: ['page'],
-        });
+        super();
         this.form = this.createForm();
         this.addChild([this.form]);
     }
@@ -20,13 +18,13 @@ export default class LoginPage extends BaseComponent {
             'Login',
             (e) => {
                 e.preventDefault();
-                console.log('login');
+                this.notifyObservers('login');
             },
             'submit'
         );
         const infoBtn = new Button(['form_btn'], 'About', (e) => {
             e.preventDefault();
-            console.log('about');
+            this.notifyObservers('showInfo');
         });
         const login = new LoginInput('User name', 3, 'userLogin', 'text');
 
@@ -39,5 +37,13 @@ export default class LoginPage extends BaseComponent {
             'Password must contain at least one capital letter and a digit'
         );
         return new LoginForm([login, password], [loginBtn, infoBtn]);
+    }
+
+    getUser() {
+        const userIputs = this.form.getInputValues();
+        const userData = userIputs.reduce((acc, inputData) => {
+            return { ...acc, ...inputData };
+        }, {});
+        return userData;
     }
 }
