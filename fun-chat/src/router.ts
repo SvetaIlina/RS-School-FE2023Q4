43c2c869f1d7b176fce7e205d0d3e66cc9ec7d08
Svetlana Ilina <svetailina1987@gmail.com>
2024-04-15 17:24:40 +0400

    feat: prevent authorized users from going to the login page

--- fun-chat/src/router.ts
+++ fun-chat/src/router.ts
@@ -4,15 +4,17 @@ import MainView from './view';
 export default class Router {
     private storageKey: string = 'myUser';
     private manager: MainView;
+    private currentPage: string;
     constructor(pageManager: MainView) {
         this.manager = pageManager;
         window.addEventListener('hashchange', () => {
             this.locationHandler();
         });
+        this.currentPage = '';
     }
     init() {
         let hash;
-        this.checkUser(this.storageKey) ? (hash = PageIds.MainPage) : (hash = window.location.hash);
+        this.checkUser(this.storageKey) ? (hash = PageIds.MainPage) : (hash = PageIds.LoginPage);
 
         this.route(hash);
     }
@@ -24,12 +26,18 @@ export default class Router {
 
     locationHandler() {
         let hash = window.location.hash.slice(1);
+
         const isAuthenticated = this.checkUser(this.storageKey);
         if (!isAuthenticated && hash === PageIds.MainPage) {
             window.location.hash = PageIds.LoginPage;
             alert('need log in');
+            return;
+        }
+        if (this.currentPage === PageIds.MainPage && hash === PageIds.LoginPage) {
+            window.location.hash = PageIds.MainPage;
         }
         this.manager.setContent(hash);
+        this.currentPage = hash;
     }
 
     checkUser(key: string): boolean {
