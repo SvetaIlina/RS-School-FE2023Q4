267444c7ecab5ade8225edffe5e5d04a14b8bb1d
Svetlana Ilina <svetailina1987@gmail.com>
2024-04-18 14:48:14 +0400

    feat: add dialogInput component

--- fun-chat/src/components/chat/dialog/dialog.ts
+++ fun-chat/src/components/chat/dialog/dialog.ts
@@ -2,17 +2,19 @@ import BaseComponent from '../../baseComponent';
 import ChatData from '../chatData';
 import DialogHeader from './dialoHeader/dialogHeader';
 import './dialog.css';
+import DialogInput from './dialogInput/dialogInput';
 import DialogMessages from './dialogMessage/dialogMessage';
 
 export default class Dialog extends BaseComponent {
     private header: DialogHeader = new DialogHeader();
     private messages: DialogMessages = new DialogMessages();
+    private messageInput: DialogInput = new DialogInput();
     constructor() {
         super({
             tag: 'div',
             classes: ['dialog'],
         });
-        this.addChild([this.header, this.messages]);
+        this.addChild([this.header, this.messages, this.messageInput]);
     }
 
     updateHeader(newUser: thirdPartyUser) {
---
+++ fun-chat/src/components/chat/dialog/dialogInput/dialogInput.ts
@@ -0,0 +1,42 @@
+import { isNotNullElement } from '../../../../servise/servise';
+import BaseComponent from '../../../baseComponent';
+import Button from '../../../buttons/button';
+import './dialogInput.css';
+
+export default class DialogInput extends BaseComponent {
+    private sendBtn: Button;
+    private messageInput: BaseComponent<HTMLInputElement> = new BaseComponent<HTMLInputElement>({
+        tag: 'input',
+        classes: ['message_input'],
+        attributes: [{ key: 'placeholder', value: 'message...' }],
+    });
+    constructor() {
+        super({
+            tag: 'div',
+            classes: ['dialog_input'],
+        });
+        this.sendBtn = new Button(['send_btn', 'inactive'], 'send', (e) => this.sendedMessage(e));
+        this.messageInput.setCallback(() => this.enableBtn(), 'keyup');
+        this.addChild([this.messageInput, this.sendBtn]);
+    }
+
+    sendedMessage(e: Event) {
+        const btn = e.target;
+        isNotNullElement<HTMLElement>(btn);
+        const input = this.messageInput.getElement();
+        const message = input.value;
+        message ? console.log('send') : console.log('nothing');
+        input.value = '';
+        btn.classList.add('inactive');
+    }
+
+    enableBtn() {
+        const input = this.messageInput.getElement();
+        const btn = this.sendBtn.getElement();
+        if (input.value) {
+            btn.classList.remove('inactive');
+        } else {
+            btn.classList.add('inactive');
+        }
+    }
+}
