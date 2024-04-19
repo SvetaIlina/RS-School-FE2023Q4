9fbfda0a4c49a91662e4d809d53a1c98283ec2f1
Svetlana Ilina <svetailina1987@gmail.com>
2024-04-19 16:28:39 +0400

    feat: add dynamic displaying userName

--- fun-chat/src/pages/mainPage/mainPageView.ts
+++ fun-chat/src/pages/mainPage/mainPageView.ts
@@ -1,3 +1,4 @@
+import Chat from '../../components/chat/chat';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 import BasePage from '../basePage';
@@ -5,15 +6,18 @@ import BasePage from '../basePage';
 export default class mainPage extends BasePage {
     header: Header;
     footer: Footer;
-    constructor() {
+    chat: Chat;
+    constructor(userName: string) {
         super();
         this.header = new Header(
-            'sweta',
+            `${userName}`,
             'fun Chat',
             () => this.notifyObservers('logOut'),
             () => this.notifyObservers('showInfo')
         );
         this.footer = new Footer();
-        this.addChild([this.header, this.footer]);
+        this.chat = new Chat([{ login: 'www', isLogined: false }]);
+
+        this.addChild([this.header, this.chat, this.footer]);
     }
 }
