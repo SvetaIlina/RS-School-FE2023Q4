41af98cad1acb61e48638caa8600ff149cddee53
Svetlana Ilina <svetailina1987@gmail.com>
2024-04-19 16:26:47 +0400

    feat: add new methods for checking and setting user

--- fun-chat/src/components/chat/chatData.ts
+++ fun-chat/src/components/chat/chatData.ts
@@ -2,20 +2,41 @@ import { isNotNull } from '../../servise/servise';
 import Dialog from './dialog/dialog';
 
 export default class ChatData {
-    dialog: Dialog;
-    contacts: Array<thirdPartyUser>;
+    dialog: Dialog | null;
+    contacts: Array<thirdPartyUser> | null;
     currentContact: thirdPartyUser | null;
-    constructor(contactList: Array<thirdPartyUser>, dialog: Dialog) {
-        this.dialog = dialog;
-        this.contacts = contactList;
+    myUser: currentUser | null;
+    private storageKey: string = 'myUser';
+    isLogined: boolean;
+    constructor() {
+        this.dialog = null;
+        this.contacts = null;
         this.currentContact = null;
+        this.myUser = null;
+        this.isLogined = false;
     }
+    // constructor(contactList: Array<thirdPartyUser>, dialog: Dialog) {
+    //     this.dialog = dialog;
+    //     this.contacts = contactList;
+    //     this.currentContact = null;
+    // }
 
     updateSelectedContact(contactName: string | null) {
         isNotNull(contactName);
+        isNotNull(this.contacts);
+        isNotNull(this.dialog);
         const contact = this.contacts.find((item) => item.login === contactName);
         isNotNull(contact);
         this.currentContact = contact;
         this.dialog.updateHeader(contact);
+    }
+
+    setMyUser() {
+        sessionStorage.setItem(this.storageKey, JSON.stringify(this.myUser));
+        this.isLogined = true;
+    }
+
+    checkUser(): boolean {
+        return sessionStorage.getItem(this.storageKey) ? true : false;
     }
 }
