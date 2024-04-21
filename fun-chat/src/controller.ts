f0a7d3a7abac1031f522aa3446e3c82b325386e2
Svetlana Ilina <svetailina1987@gmail.com>
2024-04-21 14:19:55 +0400

    feat: add cheking authorazed user

--- fun-chat/src/controller.ts
+++ fun-chat/src/controller.ts
@@ -15,7 +15,12 @@ export default class Controller {
         this.model = model;
         this.view = view;
         this.view.subscribe(this);
-        this.ws = new MyWebSocket(url, this.showConnectMessage.bind(this), this.handleWebSocketMessage.bind(this));
+        this.ws = new MyWebSocket(
+            url,
+            this.handleWebSocketOnopen.bind(this),
+            this.handleWebSocketMessage.bind(this),
+            this.handleWebSocketOnClose.bind(this)
+        );
         this.router = new Router(this, this.model);
     }
     init() {
@@ -26,12 +31,17 @@ export default class Controller {
         this.ws.initWebSocket();
     }
 
-    showConnectMessage(isOpen: boolean): void {
-        this.view.showModal(ConnectMessage.Ready, isOpen);
+    handleWebSocketOnopen(isOpen: boolean, message: string): void {
+        this.getAllUsers.call(this, messageType.ActiveUser);
+        this.getAllUsers.call(this, messageType.InactiveUser);
+        this.view.showModal(message, isOpen);
+    }
+
+    handleWebSocketOnClose(isOpen: boolean, message: string) {
+        this.view.showModal(message, isOpen);
     }
 
     handleWebSocketMessage(e: MessageEvent) {
-        let errorMessage: string;
         const dataFromServer: generalRequest | errorResponse = JSON.parse(e.data);
         const type = dataFromServer.type;
 
@@ -47,12 +57,29 @@ export default class Controller {
                 this.router.route(PageIds.LoginPage);
                 break;
             }
-            case messageType.Error: {
-                if ('error' in dataFromServer.payload) {
-                    errorMessage = dataFromServer.payload.error;
+            case messageType.ActiveUser: {
+                const users = this.checkServerData(dataFromServer, 'users');
+                if (Array.isArray(users)) {
+                    this.model.ActiveUser = users as thirdPartyUser[];
+                }
 
+                break;
+            }
+            case messageType.InactiveUser: {
+                const users = this.checkServerData(dataFromServer, 'users');
+                if (Array.isArray(users)) {
+                    this.model.inActiveUser = users as thirdPartyUser[];
+                }
+
+                break;
+            }
+            case messageType.Error: {
+                const errorMessage = this.checkServerData(dataFromServer, 'error');
+                if (typeof errorMessage === 'string') {
                     this.view.showModal(errorMessage, this.ws.isOpen);
                 }
+
+                break;
             }
         }
     }
@@ -85,17 +112,36 @@ export default class Controller {
 
     authorizeUser(data: string) {
         const user = JSON.parse(data);
+        this.model.myUser = user;
+        const alreadyLogined = Boolean(this.model.checkLogginedUser());
+        if (!alreadyLogined) {
+            console.log(this.model.ActiveUser);
+
+            const serverRequest: generalRequest = {
+                id: crypto.randomUUID(),
+                type: messageType.Login,
+                payload: {
+                    user: {
+                        login: user.login,
+                        password: user.password,
+                    },
+                },
+            };
+            this.ws.sendRequest(serverRequest);
+        } else {
+            const modalMessage = 'User is already authorize';
+            this.model.myUser = null;
+            this.view.showModal(modalMessage, this.ws.isOpen);
+        }
+    }
+
+    getAllUsers(userStatus: string) {
         const serverRequest: generalRequest = {
             id: crypto.randomUUID(),
-            type: messageType.Login,
-            payload: {
-                user: {
-                    login: user.login,
-                    password: user.password,
-                },
-            },
+            type: userStatus,
+            payload: null,
         };
-        this.model.myUser = user;
+
         this.ws.sendRequest(serverRequest);
     }
 
@@ -113,5 +159,19 @@ export default class Controller {
             },
         };
         this.ws.sendRequest(serverRequest);
+    }
+
+    checkServerData(dataFromServer: generalRequest | errorResponse, checkingType: string) {
+        isNotNull(dataFromServer.payload);
+        const payload = dataFromServer.payload;
+
+        if ('users' in payload && checkingType === 'users') {
+            return payload.users;
+        } else if ('message' in payload && checkingType === 'message') {
+            return payload.message;
+        } else if ('error' in payload && checkingType === 'error') {
+            return payload.error;
+        }
+        return null;
     }
 }
