a802e62401f7573a94ebc6b29f88bf8e65be8a9b
Svetlana <svetailina1987@gmail.com>
2024-09-17 14:08:53 +0300

    fix

--- fun-chat/src/index.ts
+++ fun-chat/src/index.ts
@@ -1,7 +1,7 @@
 import App from './app/app';
 import './style.css';
 
-const url = 'ws://fun-chat-server-production-83cf.up.railway.app';
+const url = 'wss://fun-chat-server-production-83cf.up.railway.app';
 const app = new App(url);
 
 app.init();
