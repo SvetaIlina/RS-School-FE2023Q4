2deab4444b1acd33de63a96158873ac73d195dd4
Svetlana <svetailina1987@gmail.com>
2024-09-17 14:00:07 +0300

    feat: add new url

--- fun-chat/src/index.ts
+++ fun-chat/src/index.ts
@@ -1,7 +1,7 @@
 import App from './app/app';
 import './style.css';
 
-const url = 'ws://localhost:4000';
+const url = 'ws://fun-chat-server-production-83cf.up.railway.app';
 const app = new App(url);
 
 app.init();
